9e6d934896591aaa0c3d2dfc9f2b77df161fbf67
Erwin-Cino <ErwinCino0812@gmail.com>
2019-09-03 23:04:45 -0700

    Complex Objects. Ojbect inside an array JSON

---
+++ js/complexObjects.js
@@ -0,0 +1,22 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }, {
+    "artist": "erwin",
+    "title":"butterscotch",
+    "release_year": 1995,
+    "formats": [
+      "Rock",
+      "Jazz"
+    ]
+  }
+  // Add record here
+];
