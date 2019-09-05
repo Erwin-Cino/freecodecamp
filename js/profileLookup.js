ff56be06c75f6b25bfa154da39dfc7adb2591507
Erwin-Cino <ErwinCino0812@gmail.com>
2019-09-05 07:32:40 -0700

    profileLookupJS

---
+++ js/profileLookup.js
@@ -0,0 +1,52 @@
+//Setup
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+
+
+function lookUpProfile(name, prop){
+// Only change code below this line
+  var nameProp = 0;
+  for (var i = 0; i < contacts.length; i++) {
+
+      if (contacts[i].firstName === name) {
+        if (contacts[i].hasOwnProperty(prop)) {
+          nameProp = contacts[i][prop];
+        } else if (!contacts[i].hasOwnProperty(prop)) {
+          nameProp = "No such property";
+        }
+        return nameProp;
+    } else {
+      nameProp = "No such contact";
+    }
+  }  
+  return nameProp;
+// Only change code above this line
+}
+
+
+// Change these values to test your function
+lookUpProfile("Bob", "number");
