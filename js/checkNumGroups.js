31643dc33e8f4f1e9c084b34efa4a0f1bbff2751
Erwin-Cino <ErwinCino0812@gmail.com>
2019-09-10 04:05:33 -0700

    checkNumGroups.js

---
+++ js/checkNumGroups.js
@@ -0,0 +1,6 @@
+let repeatNum = "42 42 42";
+let reRegex = /^(\d+)\s\1\s\1$/; // Change this line
+let result = reRegex.test(repeatNum);
+console.log(result);
+let matchResult = repeatNum.match(reRegex);
+console.log(matchResult);
