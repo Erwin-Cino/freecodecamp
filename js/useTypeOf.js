bc4f6df28a6a65a9d04afaa16d9ca3d2566eda20
Erwin-Cino <ErwinCino0812@gmail.com>
2019-09-15 00:13:28 -0700

    useTypeOf.js

---
+++ js/useTypeOf.js
@@ -0,0 +1,8 @@
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  let result;
+  result = typeof(bool) === 'boolean';
+  return result;
+}
+
+booWho(null);
