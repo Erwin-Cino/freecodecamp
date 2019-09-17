794b7f299eb671fb703b4343bbc2df445a4c61bf
Erwin-Cino <ErwinCino0812@gmail.com>
2019-09-16 19:15:03 -0700

    mutations js solution

--- js/mutations.js
+++ js/mutations.js
@@ -1,31 +1,12 @@
 function mutation(arr) {
-  let result;
-  // arr.map(x => {
-  //   x = x.split('');
-  //   console.log(x)
-  // })
-  let newArr = [];
-  let stringFirst;
-  let i = 0;
-  stringFirst = arr[i].split('').map(x => {
-    newArr.push(x.toLowerCase());
-  })
-  console.log(newArr);
   
-  return result
+  let first = arr[0].toLowerCase();
+  let second = arr[1].toLowerCase();
+  
+  for (let i = 0; i < second.length; i++) {
+    if (first.indexOf(second[i]) < 0) return false;
+  }
+  return true;
+}
 
-  // stringFirst = arr[0].split('').map(x => {
-  //   x.toLowerCase(i);
-  //   i++;
-  //   arr[1].split('').map(y => {
-  //     y.toLowerCase(i);
-  //      if (x == y) {
-  //        console.log(x);
-  //        result = true;
-  //      } else {
-  //        result = false;
-  //      }
-  //   })
-  // });
-
-mutation(["ZyxwVutsrqponmlkjihgfedcba", "qrstu"]);
+mutation(["hello", "hey"]);
