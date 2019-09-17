fdbc748c0588b0ae725d55e0ecb20b7a0b248cc1
Erwin-Cino <ErwinCino0812@gmail.com>
2019-09-16 20:51:14 -0700

    chunkeyMonkey.js

---
+++ js/chunkeyMonkey.js
@@ -0,0 +1,21 @@
+function chunkArrayInGroups(arr, size) {
+  // Break it up.
+  let temp = [];
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i % size !== size - 1) {
+      temp.push(arr[i]);
+    } else {
+      temp.push(arr[i]);
+      result.push(temp);
+      temp = [];
+    }
+  }
+    if (temp.length !== 0) {
+      result.push(temp);
+    } 
+  return result;
+  
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
