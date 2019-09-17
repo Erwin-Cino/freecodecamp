7aa95c86a93e645f46d6d613b5e9c4eb3bb1d7fe
Erwin-Cino <ErwinCino0812@gmail.com>
2019-09-16 20:55:54 -0700

    chunkyMonkey.js shorter solution

--- js/chunkeyMonkey.js
+++ js/chunkeyMonkey.js
@@ -19,3 +19,15 @@ function chunkArrayInGroups(arr, size) {
 }
 
 chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
+function chunkArrayInGroups(arr, size) {
+  // Break it up.
+  let result = [];
+  for (let i = 0; i < arr.length; i += size) {
+    result.push(arr.slice(i, i + size));
+  }
+  console.log(result);
+  return result;
+}
+
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4);
