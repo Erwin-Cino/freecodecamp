9e3f6e57bad542fade6811830a61d34af5536d13
Erwin-Cino <ErwinCino0812@gmail.com>
2019-10-02 16:44:11 -0700

    comments in ReactJS

---
+++ ReactJS/jsxReactDiv.js
@@ -0,0 +1,11 @@
+// write your code here
+
+const JSX = <div>
+                <h1>List Title</h1>
+                <p>List Description</p>
+                <ul>
+                    <li>1st person</li>
+                    <li>2nd person</li>
+                    <li>3rd person</li>
+                </ul>
+            </div>
