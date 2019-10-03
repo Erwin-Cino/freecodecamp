141abac5e8b3c2aa54abf4ea5904bab888bedd04
Erwin-Cino <ErwinCino0812@gmail.com>
2019-10-02 17:22:08 -0700

    functionComponentReact

---
+++ ReactJS/functionComponentReact.js
@@ -0,0 +1,11 @@
+const MyComponent = function() {
+  // change code below this line
+  return (
+    <div>
+      <p>This is where I write my paragraph</p>
+    </div>
+    )
+
+
+  // change code above this line
+}
