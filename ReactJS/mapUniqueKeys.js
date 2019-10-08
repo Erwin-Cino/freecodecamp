0386255445c8fd6f754971ae301b888cb1e4add2
Erwin-Cino <ErwinCino0812@gmail.com>
2019-10-09 07:38:05 +0800

    Used map to render list and added unique key values per loop

---
+++ ReactJS/mapUniqueKeys.js
@@ -0,0 +1,24 @@
+
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
+
+function Frameworks() {
+  const renderFrameworks = frontEndFrameworks.map(list => {
+    const pos = frontEndFrameworks.indexOf(list);
+    return  <li key={pos}>{list}</li>
+  }); // change code here
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
+    </div>
+  );
+};
