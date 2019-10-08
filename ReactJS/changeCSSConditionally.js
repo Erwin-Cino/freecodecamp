9b2eb0996b355e9c5a4bd64fcaba206349f3fa1e
Erwin-Cino <ErwinCino0812@gmail.com>
2019-10-09 06:19:59 +0800

    Change CSS style if the user input more than 15 characters

---
+++ ReactJS/changeCSSConditionally.js
@@ -0,0 +1,34 @@
+
+
+class GateKeeper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ input: event.target.value })
+  }
+  render() {
+    let inputStyle = {
+      border: '1px solid black'
+    };
+    // change code below this line
+    console.log(inputStyle.border)
+    console.log(this.state.input.length)
+    this.state.input.length > 15 ? inputStyle.border = "3px solid red" : null
+    // change code above this line
+    return (
+      <div>
+        <h3>Don't Type Too Much:</h3>
+        <input
+          type="text"
+          style={inputStyle}
+          value={this.state.input}
+          onChange={this.handleChange} />
+      </div>
+    );
+  }
+};
